1b7a46fd1bbfda1b485e4f7717f7bda2e9469e3e
Yartsev Alexander <yarick1983@gmail.com>
2020-01-08 13:10:53 +0300

    [UPD] creating first implementation of --init option

--- odpm/odpm.py
+++ odpm/odpm.py
@@ -12,6 +12,7 @@ import logging
 import zipfile
 import shutil
 import urllib
+import io
 from string import Template
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,7 +27,7 @@ parser.add_argument('--drop-db', help='db name to drop')
 parser.add_argument('--backup-db', help='db name to backup, working with opiton --arch-path')
 parser.add_argument('--restore-db', help='db name to restore, working with opiton --arch-path')
 parser.add_argument('--arch-path', help='path to database archive, working with options --backup-db and --restore-db')
-# parser.add_argument('--init', help='init project with default params it takes over 10 -15 minutes')
+parser.add_argument('--init', help='init project with current name default params and create default odoo project with default module')
 parser.add_argument('--update', type=bool, nargs='?',const=True, default=False,help='this is option will update all git repos (odoo, depends, dev_project etc...)')
 parser.add_argument('--project-path', help='this is option tell to the programm in wich project context we will work')
 
@@ -40,12 +41,14 @@ if options.project_path:
         options.project_path = options.project_path[:-1]
     project_dir = options.project_path
     
-project_config_path = "%s/%s"%(project_dir, project_config_file)
-if not os.path.exists(project_config_path):
+project_config_path = os.path.join(project_dir, project_config_file)
+if not os.path.exists(project_config_path) and not options.init:
     print(u"Перейдите в каталог где существует %s файл или укажите каталог проекта с помощью параметра --project-path '/путь/к/каталогу/с/проектом/' "%(project_config_file))
     exit()
-
-JSON_CONF = json.loads(open(project_config_path).read())
+if os.path.exists(project_config_path):
+    JSON_CONF = json.loads(open(project_config_path).read())
+else:
+    JSON_CONF ={}
 
 
 if 'projects_dir' in JSON_CONF.keys():
@@ -80,10 +83,25 @@ if not os.path.exists(odoo_venvs_dir):
 
 
 
-dependencies_projects_urls =JSON_CONF['dependencies_projects_urls']
-dev_project_url = JSON_CONF['dev_project_url']
-odoo_version_for_project = JSON_CONF['odoo_version_for_project']
-project_name = JSON_CONF['project_name']
+if 'dependencies_projects_urls' not in JSON_CONF.keys():
+    dependencies_projects_urls = []
+else:
+    dependencies_projects_urls = JSON_CONF['dependencies_projects_urls']
+    
+if 'dev_project_url' not in JSON_CONF.keys():
+    dev_project_url = None
+else:
+    dev_project_url = JSON_CONF['dev_project_url']
+    
+if 'odoo_version_for_project' not in JSON_CONF.keys():
+    odoo_version_for_project = "12.0"
+else:
+    odoo_version_for_project = JSON_CONF['odoo_version_for_project']
+
+if 'project_name' not in JSON_CONF.keys():
+    project_name = None
+else:
+    project_name = JSON_CONF['project_name']
 
 if 'modules_to_update' not in JSON_CONF.keys():
     modules_to_update = None
@@ -104,66 +122,65 @@ else:
     database_name = JSON_CONF['database_name']
 
 
+if 'git_servers_params' not in JSON_CONF.keys():
+    git_servers_params = {}
+else:
+    git_servers_params = JSON_CONF['git_servers_params']
 
-git_servers_params = JSON_CONF['git_servers_params']
 
 
 
 
-    
+# def restore_db(db, dump_file, copy=False):
+#     filestore_dest = home_dir + '/.local/share/Odoo/filestore/' + db
+#     assert isinstance(db, pycompat.string_types)
+#     if odoo.service.db.exp_db_exist(db):
+#         _logger.info('RESTORE DB: %s already exists', db)
+#         raise Exception("Database %s already exists"%(db))
 
-    
+#     odoo.service.db._create_empty_database(db)
 
-def restore_db(db, dump_file, copy=False):
-    filestore_dest = home_dir + '/.local/share/Odoo/filestore/' + db
-    assert isinstance(db, pycompat.string_types)
-    if odoo.service.db.exp_db_exist(db):
-        _logger.info('RESTORE DB: %s already exists', db)
-        raise Exception("Database %s already exists"%(db))
+#     filestore_path = None
+#     with odoo.tools.osutil.tempdir() as dump_dir:
+#         if zipfile.is_zipfile(dump_file):
+#             # v8 format
+#             with zipfile.ZipFile(dump_file, 'r') as z:
+#                 # only extract known members!
+#                 filestore = [m for m in z.namelist() if m.startswith('filestore/')]
+#                 z.extractall(dump_dir, ['dump.sql'] + filestore)
 
-    odoo.service.db._create_empty_database(db)
+#                 if filestore:
+#                     filestore_path = os.path.join(dump_dir, 'filestore')
 
-    filestore_path = None
-    with odoo.tools.osutil.tempdir() as dump_dir:
-        if zipfile.is_zipfile(dump_file):
-            # v8 format
-            with zipfile.ZipFile(dump_file, 'r') as z:
-                # only extract known members!
-                filestore = [m for m in z.namelist() if m.startswith('filestore/')]
-                z.extractall(dump_dir, ['dump.sql'] + filestore)
+#             pg_cmd = 'psql'
+#             pg_args = ['-q', '-f', os.path.join(dump_dir, 'dump.sql')]
 
-                if filestore:
-                    filestore_path = os.path.join(dump_dir, 'filestore')
+#         else:
+#             # <= 7.0 format (raw pg_dump output)
+#             pg_cmd = 'pg_restore'
+#             pg_args = ['--no-owner', dump_file]
 
-            pg_cmd = 'psql'
-            pg_args = ['-q', '-f', os.path.join(dump_dir, 'dump.sql')]
+#         args = []
+#         args.append('--dbname=' + db)
+#         pg_args = args + pg_args
 
-        else:
-            # <= 7.0 format (raw pg_dump output)
-            pg_cmd = 'pg_restore'
-            pg_args = ['--no-owner', dump_file]
+#         if odoo.tools.exec_pg_command(pg_cmd, *pg_args):
+#             raise Exception("Couldn't restore database")
 
-        args = []
-        args.append('--dbname=' + db)
-        pg_args = args + pg_args
+#         registry = odoo.modules.registry.Registry.new(db)
+#         cr = registry.cursor()
+#         # env = odoo.api.Environment(cr, SUPERUSER_ID, {})
+#         if filestore_path:
+#             shutil.move(filestore_path, filestore_dest)
 
-        if odoo.tools.exec_pg_command(pg_cmd, *pg_args):
-            raise Exception("Couldn't restore database")
+#         if odoo.tools.config['unaccent']:
+#             try:
+#                 with cr.savepoint():
+#                     cr.execute("CREATE EXTENSION unaccent")
+#             except psycopg2.Error:
+#                 pass
 
-        registry = odoo.modules.registry.Registry.new(db)
-        cr = registry.cursor()
-        # env = odoo.api.Environment(cr, SUPERUSER_ID, {})
-        if filestore_path:
-            shutil.move(filestore_path, filestore_dest)
-
-        if odoo.tools.config['unaccent']:
-            try:
-                with cr.savepoint():
-                    cr.execute("CREATE EXTENSION unaccent")
-            except psycopg2.Error:
-                pass
-
-    _logger.info('RESTORE DB: %s', db)
+#     _logger.info('RESTORE DB: %s', db)
 
 
 # drop_data_base = JSON_CONF['drop_data_base']
@@ -260,25 +277,33 @@ def write_new_file(filename,content):
 def get_list_of_modules_in_project(project_path):
     current_dirs = False
     current_files = False
+    list_of_all_files = []
     for i,j,y in os.walk(project_path):
+        for elemet in y:
+            list_of_all_files.append(elemet)
         if not current_dirs:
             current_dirs = j
             current_files = y
-    return current_dirs, current_files
+    return current_dirs, current_files , list_of_all_files
     
 # Функция для которая сканирует содрежимое каталога с проектом и отдает кортеж с анализом ситуации
 def check_dir_content(full_dir_path):
     dir_is_module = False
     dir_is_git_repo = False
+    dir_has_module = False
     recources = get_list_of_modules_in_project(full_dir_path)
     dirs_list = recources[0]
     files_list = recources[1]
+    list_of_all_files_with_subdirs = recources[2]
     if files_list and '__manifest__.py' in files_list:
         dir_is_module = True
+    # print(list_of_all_files_with_subdirs)
+    if '__manifest__.py' in list_of_all_files_with_subdirs:
+        dir_has_module = True
     if dirs_list and '.git' in dirs_list:
         dir_is_git_repo = True
     
-    return dirs_list, files_list, dir_is_module, dir_is_git_repo
+    return dirs_list, files_list, dir_is_module, dir_is_git_repo, dir_has_module
     
 def modify_from_module_to_project(this_project_name):
     this_project_dir = os.path.join(odoo_projects_dir,this_project_name)
@@ -338,6 +363,21 @@ def check_if_project_exists(project_url):
         repo.git.stash()
         repo.git.checkout(odoo_version_for_project)
     return project_name_from_url
+    
+def create_dummy_odoo_module(module_name,module_path):
+    odoo_version = odoo_version_for_project.split('.')[0]
+    current_odoo_version_venv_dir = '%s/venv_odoo_%s'%(odoo_venvs_dir,odoo_version)
+    activate_this = os.path.join(current_odoo_version_venv_dir, 'bin', 'activate_this.py')
+    exec(compile(open(activate_this, "rb").read(), activate_this, 'exec'), dict(__file__=activate_this), {})
+    start_odoo_for_template_creating = Template(
+        """python${python_version} ${odoo_bin_path} scaffold -t ${module_template_path} ${module_name} ${module_path}""").substitute({
+            "python_version":python_version,
+            "odoo_bin_path":os.path.join(odoo_dir,'odoo-bin'),
+            "module_template_path":os.path.join(script_dir,'templates/default'),
+            "module_path":module_path,
+            "module_name":module_name,
+        })
+    run_command(start_odoo_for_template_creating)
 
 def main():
     # check odoo repo
@@ -351,6 +391,8 @@ def main():
 if __name__ == "__main__":
     main()
     
+
+    
 if not os.path.exists(odoo_projects_dir):
     run_command('mkdir -p %s'%(odoo_projects_dir))
 dependencies_projects_names_list = []
@@ -361,36 +403,65 @@ for project_url in dependencies_projects_urls:
 
 
 
+if options.init:
+    project_name = options.init
+    odoo_project_dir = os.path.join(odoo_projects_dir,project_name)
+    run_command('mkdir -p %s'%(odoo_project_dir))
+    create_dummy_odoo_module(project_name,odoo_project_dir)
+    project_dir = os.path.join(projects_dir,project_name)
+    run_command('mkdir -p %s'%(project_dir))
+    new_config_file = os.path.join(project_dir,project_config_file)
+    new_config = {
+        "dependencies_projects_urls": dependencies_projects_urls,
+        "dev_project_url": dev_project_url,
+        "odoo_version_for_project": odoo_version_for_project,
+        "project_name": project_name,
+        "modules_to_update":[project_name],
+        "git_servers_params":{
+        }
+    }
+    with io.open(new_config_file, 'w', encoding='utf8') as config_file:
+        json.dump(new_config, config_file, indent=4)
+    
+odoo_project_dir = os.path.join(odoo_projects_dir,project_name)
+
 if dev_project_url:
     project_dir_from_url = check_if_project_exists(dev_project_url)
-    if not project_name:
-        project_name = project_dir_from_url
+    odoo_project_dir = os.path.join(odoo_projects_dir,project_dir_from_url)
+    # if not project_name:
+    #     project_name = project_dir_from_url
 
-else:
-    run_command('mkdir -p %s%s'%(odoo_projects_dir,project_name))
-    project_dir_path = '%s%s/'%(odoo_projects_dir,project_name)
-    init_module_dir_path = '%s%s'%(project_dir_path,project_name)
+if not check_dir_content(odoo_project_dir)[4]:
+    print(u"There is not odoo module in project. Try to start odpm --init your_project_name")
+    exit()
+# odoo_project_dir
+
+
+# else:
+#     run_command('mkdir -p %s%s'%(odoo_projects_dir,project_name))
+#     project_dir_path = '%s%s/'%(odoo_projects_dir,project_name)
+#     init_module_dir_path = '%s%s'%(project_dir_path,project_name)
     
-    # Тут нужно будет инициировать проект
+#     # Тут нужно будет инициировать проект
 
-    if not check_dir_content(init_module_dir_path)[2]:
-        odoo_version = odoo_version_for_project.split('.')[0]
-        current_odoo_version_venv_dir = os.path.join(odoo_venvs_dir,'venv_odoo_%s'%(odoo_version))
-        activate_this = os.path.join(current_odoo_version_venv_dir, 'bin', 'activate_this.py')
-        exec(compile(open(activate_this, "rb").read(), activate_this, 'exec'), dict(__file__=activate_this), {})
-        run_command('%s/odoo-bin scaffold -t default %s %s'%(odoo_dir,project_name,project_dir_path) )
+#     if not check_dir_content(init_module_dir_path)[2]:
+#         odoo_version = odoo_version_for_project.split('.')[0]
+#         current_odoo_version_venv_dir = os.path.join(odoo_venvs_dir,'venv_odoo_%s'%(odoo_version))
+#         activate_this = os.path.join(current_odoo_version_venv_dir, 'bin', 'activate_this.py')
+#         exec(compile(open(activate_this, "rb").read(), activate_this, 'exec'), dict(__file__=activate_this), {})
+#         run_command('%s/odoo-bin scaffold -t default %s %s'%(odoo_dir,project_name,project_dir_path) )
     
-    # run_command('mkdir -p %s/%s'%(projects_dir,project_name))
+    # 
 
-if not dev_project_url:
-    project_dir_from_url = project_name
+# if not dev_project_url:
+#     project_dir_from_url = project_name
 
 
 if check_dir_content(odoo_dir)[3]:
     checkout_git_repo(odoo_dir,odoo_version_for_project)
 
 if not modules_to_update:
-    modules_to_update =[project_dir_from_url]
+    modules_to_update =[project_name]
 
 string_modules_to_update = ""
 for module_name  in modules_to_update:
@@ -403,21 +474,21 @@ if not database_name:
 
 
 
-odoo_modules_dir = os.path.join(projects_dir,project_name,'odoo_modules')
+project_odoo_modules_dir = os.path.join(projects_dir,project_name,'odoo_modules')
 # if not os.path.exists(odoo_modules_dir):
 #     os.makedirs(odoo_modules_dir, exist_ok=True)
 
-run_command('rm -rf %s'%(odoo_modules_dir))
-run_command('ln -s %s %s'%(os.path.join(odoo_projects_dir,project_dir_from_url),odoo_modules_dir))
+run_command('rm -rf %s'%(project_odoo_modules_dir))
+run_command('ln -s %s %s'%(odoo_project_dir,project_odoo_modules_dir))
 
 addons_string = ''
 for project in dependencies_projects_names_list:
     if os.path.exists(os.path.join(odoo_projects_dir,project)):
         addons_string = addons_string + os.path.join(odoo_projects_dir,project) + ","
 
-if project_dir_from_url:
-    if os.path.exists(os.path.join(odoo_projects_dir,project_dir_from_url)):
-        addons_string = addons_string + os.path.join(odoo_projects_dir,project_dir_from_url) + ","
+# if project_dir_from_url:
+if os.path.exists(odoo_project_dir):
+    addons_string = addons_string + odoo_project_dir + ","
 
 odoo_addons_dir = os.path.join(odoo_dir,'addons')
 odoo_odoo_addons_dir = os.path.join(odoo_dir,'odoo/addons')
@@ -512,19 +583,8 @@ if options.dev_restart:
     result=process.communicate()
 
 
+
+
 if options.create_module:
     module_name = options.create_module
-    # module_path = os.path.join(odoo_modules_dir,module_name)
-    odoo_version = odoo_version_for_project.split('.')[0]
-    current_odoo_version_venv_dir = '%s/venv_odoo_%s'%(odoo_venvs_dir,odoo_version)
-    activate_this = os.path.join(current_odoo_version_venv_dir, 'bin', 'activate_this.py')
-    exec(compile(open(activate_this, "rb").read(), activate_this, 'exec'), dict(__file__=activate_this), {})
-    start_odoo_for_template_creating = Template(
-        """python${python_version} ${odoo_bin_path} scaffold -t ${module_template_path} ${module_name} ${module_path}""").substitute({
-            "python_version":python_version,
-            "odoo_bin_path":os.path.join(odoo_dir,'odoo-bin'),
-            "module_template_path":os.path.join(script_dir,'templates/default'),
-            "module_path":odoo_modules_dir,
-            "module_name":module_name,
-        })
-    run_command(start_odoo_for_template_creating)
+    create_dummy_odoo_module(moule_name,project_odoo_modules_dir)
